440f74182de4e667ac872f33213737328772ccc9
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-01-27 08:34:13 -0300

    01_fundamentals - booklist - css stylling

--- 01-react-fundamentals/booklist/src/index.js
+++ 01-react-fundamentals/booklist/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
+import './index.css';
 
 const BookList = () => {
   return (
-    <section>
+    <section className='bookList'>
       <Book />
       <Book />
       <Book />
@@ -14,7 +15,7 @@ const BookList = () => {
 
 const Book = () => {
   return (
-    <article>
+    <article className='book'>
       <Image />
       <Title />
       <Author />
