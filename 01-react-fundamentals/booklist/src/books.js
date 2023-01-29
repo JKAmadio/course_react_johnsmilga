24a281c70bf8b43a111fca4d9f57e7c16c9559d2
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-01-29 13:56:09 -0300

    01_fundamentals - booklist - splitting code

---
+++ 01-react-fundamentals/booklist/src/books.js
@@ -0,0 +1,14 @@
+export const books = [
+  {
+    author: 'Rick Rubin',
+    title: 'The Creative Act: A Way of Being',
+    img: './images/book-1.jpg',
+    id: 1,
+  },
+  {
+    author: 'Robert Greene',
+    title: 'Nova o 48 Leis do Poder',
+    img: './images/book-2.jpg',
+    id: 2,
+  },
+];
--- 01-react-fundamentals/booklist/src/index.js
+++ 01-react-fundamentals/booklist/src/index.js
@@ -2,56 +2,16 @@ import React from 'react';
 import ReactDom from 'react-dom/client';
 import './index.css';
 
-const books = [
-  {
-    author: 'Rick Rubin',
-    title: 'The Creative Act: A Way of Being',
-    img: './images/book-1.jpg',
-    id: 1,
-  },
-  {
-    author: 'Robert Greene',
-    title: 'Nova o 48 Leis do Poder',
-    img: './images/book-2.jpg',
-    id: 2,
-  },
-];
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
-  const getBook = (id) => {
-    const book = books.find((item) => item['id'] === id);
-    console.log(book);
-  };
   return (
     <section className="bookList">
       {books.map((book) => {
-        // we use the spread operator to pass all book's infos as props
-        return <Book {...book} getBook={getBook} key={book.id} />;
+        return <Book {...book} key={book.id} />;
       })}
     </section>
-  );
-};
-
-const Book = (props) => {
-  const { title, img, author, id, getBook } = props;
-
-  /* const getSingleBook = () => {
-    getBook(id);
-  }; */
-  return (
-    <article className="book">
-      <img src={img} alt={title} />
-      {/*
-        we can use a auxiliar function to evolke the getBook at the right moment
-        <button onClick={getSingleBook}>click me</button>
-      */}
-
-      {/* or call it as an anonymous function */}
-      <button onClick={() => getBook(id)}>click me</button>
-
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
