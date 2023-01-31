0a863b8efdd40e787da97eaa5476d65677e9ae84
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-01-31 19:29:30 -0300

    02_backroads - refactoring Tours items

--- 02-backroads-application/src/data.js
+++ 02-backroads-application/src/data.js
@@ -1,3 +1,8 @@
+import tibetImg from './images/tour-1.jpeg';
+import javaImg from './images/tour-2.jpeg';
+import hongkongImg from './images/tour-3.jpeg';
+import kenyaImg from './images/tour-4.jpeg';
+
 export const pageLinks = [
   { id: 1, href: '#home', text: 'home' },
   { id: 2, href: '#about', text: 'about' },
@@ -29,5 +34,48 @@ export const services = [
     icon: 'fas fa-socks fa-fw',
     title: 'amazing comfort',
     text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.',
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    img: tibetImg,
+    date: 'august 26th, 2020',
+    title: 'Tibet Adventure',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'china',
+    interval: 6,
+    price: 2100,
+  },
+  {
+    id: 2,
+    img: javaImg,
+    date: 'october 1th, 2020',
+    title: 'Best of Java',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'indonesia',
+    interval: 11,
+    price: 1400,
+  },
+  {
+    id: 3,
+    img: hongkongImg,
+    date: 'september 15th, 2020',
+    title: 'Explore Hong Kong',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'hong kong',
+    interval: 8,
+    price: 5000,
+  },
+  {
+    id: 4,
+    img: kenyaImg,
+    date: 'december 5th, 2019',
+    title: 'Kenya Highlights',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    country: 'kenya',
+    interval: 20,
+    price: 3300,
   },
 ];
