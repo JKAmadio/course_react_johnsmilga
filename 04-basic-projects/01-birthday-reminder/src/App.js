1dc6571750b936b3805f58016226a4cc81fc7575
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-02-01 20:20:29 -0300

    04_projects - birthday reminder - done

--- 04-basic-projects/01-birthday-reminder/src/App.js
+++ 04-basic-projects/01-birthday-reminder/src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [birthdays, setBirthdays] = useState(data);
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{birthdays.length} birthdays today</h3>
+        <List birthdays={birthdays} />
+        <button onClick={() => setBirthdays([])}>Clear All</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
