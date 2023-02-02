36511ffc9ff0e04c87139c90fbe8d0484066462e
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-02-02 19:46:40 -0300

    03_adv-react - useEffect basics

--- 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,33 @@
+import { useState, useEffect } from 'react';
+
 const url = 'https://api.github.com/users';
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h2>fetch data example</h2>
+      <ul className="users">
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              <img src={user.avatar_url} />
+              <div>
+                <h5>{user.login}</h5>
+                <a href={user.html_url}>Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 export default FetchData;
