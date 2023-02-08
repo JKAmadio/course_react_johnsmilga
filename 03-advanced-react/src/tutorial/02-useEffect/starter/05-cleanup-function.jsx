88d2170da8c586bc231f0bb65ef6bf3528270b3f
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2023-02-07 23:09:39 -0300

    03_adv-react - cleanup function

--- 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,39 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <div>{toggle && <SecondCOmponent />}</div>
+      <button className="btn" onClick={() => setToggle(!toggle)}>
+        toggle
+      </button>
+    </div>
+  );
+};
+
+const SecondCOmponent = () => {
+  useEffect(() => {
+    // will be launched everytime the toggle is On and the component is mounted
+    console.log('second component');
+
+    let num = 1;
+
+    // will be launched everytime the toggle is On and the component is mounted
+    // and will keep consoling forever, even when the toggle is off
+    const intId = setInterval(() => {
+      console.log('hello from interval');
+      console.log((num += 1));
+    }, 1000);
+
+    return () => {
+      // will desable the interval funcionality when the component is unmounted
+      clearInterval(intId);
+    };
+  }, []);
+
+  return <h2>ligado</h2>;
 };
 
 export default CleanupFunction;
