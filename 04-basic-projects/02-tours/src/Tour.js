fd6e495fad6c114cada98290adaa95b32f98cce6
JKAmadio <jkahvedjian@gmail.com>
2023-02-08 20:10:54 -0300

    04_projects - tours - show more/less tour text

--- 04-basic-projects/02-tours/src/Tour.js
+++ 04-basic-projects/02-tours/src/Tour.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 
 const Tour = ({ id, image, info, name, price }) => {
+  const [showAll, setShowAll] = useState(false)
+
   return <article className="single-tour">
     <img src={image} alt=""/>
     <footer className="tour-info">
       <span className="tour-price">{price}</span>
       <h4>{name}</h4>
-      <p>{info}</p>
+        {showAll ?
+          <p>{info}
+            <button onClick={() => setShowAll(false)}>Show less</button>
+          </p> :
+          <p>{info.slice(0, 170)}...
+            <button onClick={() => setShowAll(true)}>Read more</button>
+          </p>
+        }
     </footer>
   </article>;
 };
