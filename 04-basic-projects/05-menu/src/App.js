38fbaf63a0d31bb6a2ae5a68abc39cadb180c8a3
JKAmadio <jkahvedjian@gmail.com>
2023-02-13 21:58:23 -0300

    04_project - menu - structure

--- 04-basic-projects/05-menu/src/App.js
+++ 04-basic-projects/05-menu/src/App.js
@@ -4,7 +4,32 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>
+  const [showList, setShowList] = useState(items)
+ 
+  let listinha = ['all']
+  
+  items.map(item => {
+    if (!listinha.includes(item['category']))
+      listinha.push(item['category'])
+  })
+
+  const chooseCategory = (category) => {
+    if (category === 'all')
+      setShowList(items)
+    else {
+      const newList = items.filter(item => item.category === category);
+      setShowList(newList);
+    }
+  }
+
+  return <main>
+    {listinha.map((item, index) => {
+      return <button key={index} onClick={() => chooseCategory(item)}>{item}</button>
+    })}
+    {showList.map(item => {
+      return <div key={item.id}>{item.title}</div>
+    })}
+  </main>
 }
 
 export default App;
